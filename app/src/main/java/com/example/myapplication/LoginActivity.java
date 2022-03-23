1f1d358cc8c934ebb1be52afd1e85a1641be984f
Lszlosarczyk <l.szlosarczyk19@gmail.com>
2022-03-24 00:08:53 +0100

    BoostYourCar APP

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -2,7 +2,13 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +16,30 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        TextView login = (TextView) findViewById(R.id.login);
+        TextView haslo = (TextView) findViewById(R.id.haslo);
+
+
+        MaterialButton przycisk_login = (MaterialButton) findViewById(R.id.przycisk_login);
+
+
+        przycisk_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (login.getText().toString().equals("admin") && haslo.getText().toString().equals("admin")) {
+                    Toast.makeText(LoginActivity.this, "LOGOWANIE POWIODLO SIE", Toast.LENGTH_SHORT).show();
+                    openMainActivity();
+                } else
+                    Toast.makeText(LoginActivity.this, "LOGOWANIE NIE POWIODLO SIE", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        }
+    public void openMainActivity() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
+
+
