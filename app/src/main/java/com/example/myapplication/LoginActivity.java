8e58bb349d6db391db24926a200cfce1c9f89d96
slakrusis <slawomir.krulik@sisindustrials.com>
2022-03-24 09:25:01 +0100

    add MainActivity2

--- app/src/main/java/com/example/myapplication/LoginActivity.java
+++ app/src/main/java/com/example/myapplication/LoginActivity.java
@@ -37,7 +37,7 @@ public class LoginActivity extends AppCompatActivity {
 
         }
     public void openMainActivity() {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, MainActivity2.class);
         startActivity(intent);
     }
 }
