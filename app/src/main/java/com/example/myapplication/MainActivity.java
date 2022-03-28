914582645402cbdbe3c63d9b3aa63e51aa8d821e
slakrusis <slawomir.krulik@sisindustrials.com>
2022-03-28 09:53:01 +0200

    add side menu - test

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,25 +1,64 @@
 package com.example.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
 
-import com.example.myapplication.databinding.ActivityDashboardBinding;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 
-public class MainActivity extends DrawerBaseActivity
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity
 {
-    ActivityDashboardBinding activityDashboardBinding;
+    DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        activityDashboardBinding = ActivityDashboardBinding.inflate(getLayoutInflater());
-        setContentView(activityDashboardBinding.getRoot());
+        setContentView(R.layout.activity_main);
+
+        drawer = findViewById(R.id.drawer);
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
+        {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem)
+            {
+                Log.d("TAG", "onNavigationItemSelected: " + menuItem.getItemId());
+
+                switch (menuItem.getItemId())
+                {
+                    case R.id.nav_logout:
+                        addFragment();
+                        break;
+
+                    default:
+                        break;
+                }
 
 
-        ClientHttp clientHttp = new ClientHttp();
-        clientHttp.login("","");
+                drawer.close();
+                return false;
+            }
+        });
+//         ClientHttp clientHttp = new ClientHttp();
+//         clientHttp.login("","");
 
 
     }
+
+    private void addFragment(Fragment fragment)
+    {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+
 }
