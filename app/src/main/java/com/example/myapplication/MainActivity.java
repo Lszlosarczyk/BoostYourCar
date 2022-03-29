134704d73c9cbb06b4c79bf0afa179cdb68905fc
msobota2019 <m.sobota2019@gmail.com>
2022-03-29 19:17:06 +0200

    fragmenty dzialaja dziwne

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,18 +2,22 @@ package com.example.myapplication;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.MenuItem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import com.google.android.material.navigation.NavigationView;
 
 
-public class MainActivity extends AppCompatActivity
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener
 {
     DrawerLayout drawer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,39 +27,48 @@ public class MainActivity extends AppCompatActivity
 
         drawer = findViewById(R.id.drawer);
         NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
-        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener()
-        {
-            @Override
-            public boolean onNavigationItemSelected(MenuItem menuItem)
-            {
-                Log.d("TAG", "onNavigationItemSelected: " + menuItem.getItemId());
-
-//                switch (menuItem.getItemId())
-//                {
-//                    case R.id.nav_logout:
-//                        addFragment();
-//                        break;
-//
-//                    default:
-//                        break;
-//                }
+        navView.setNavigationItemSelectedListener(this);
 
 
-                drawer.close();
-                return false;
-            }
-        });
-//         ClientHttp clientHttp = new ClientHttp();
-//         clientHttp.login("","");
+    }
 
-
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_felgi:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new FelgiFragment()).commit();
+                break;
+            case R.id.nav_tuning_silnika:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TuningSilnikaFragment()).commit();
+                break;
+            case R.id.nav_tuning_zawieszenia:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TuningZawieszeniaFragment()).commit();
+                break;
+            case R.id.nav_tuning_wewnetrzny:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TuningWewnetrznyFragment()).commit();
+                break;
+            case R.id.nav_tuning_zewnetrzny:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new TuningZewnetrznyFragment()).commit();
+                break;
+            case R.id.nav_kosmetyki_samochodowe:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new KosmetykiSamochodoweFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     private void addFragment(Fragment fragment)
     {
         getSupportFragmentManager()
                 .beginTransaction()
-                .replace(R.id.container, fragment)
+                .replace(R.id.fragment_container, fragment)
                // .addToBackStack(null)
                 .commit();
     }
